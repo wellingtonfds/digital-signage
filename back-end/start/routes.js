723c053ab5676df17e39099b1726cfa96ff8f8cc
Wellington Fernandes <wellingtonfds@gmail.com>
2019-12-11 21:01:02 -0300

    init

--- back-end/start/routes.js
+++ back-end/start/routes.js
@@ -16,4 +16,8 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.on('/').render('welcome')
+//Route.on('/').render('welcome')
+
+Route.get('/', function(){
+  return 'test';
+})
